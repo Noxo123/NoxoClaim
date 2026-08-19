291b349233b9adcb2456da187f73d4ad287016f7
Noxo123 <138624794+Noxo123@users.noreply.github.com>
2026-08-19 22:43:03 +0200

    Fix JUnit Platform runtime for CI tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "fr.noxodev"
-version = "1.1.3"
+version = "1.1.4"
 description = "NoxoClaim - claims par chunks pour Paper 26.2 et 26.1"
 
 repositories {
@@ -24,6 +24,7 @@ dependencies {
     }
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.12.2")
 }
 
 java {
